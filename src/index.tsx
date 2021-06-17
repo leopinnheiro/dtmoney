690a464a4acb26bc151a20ae14e2a5273d22e1d8
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-17 08:36:09 -0300

    Configurando miragejs e axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transacao 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Alimentação'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
