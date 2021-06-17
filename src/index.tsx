2691217b718fd2a98764d1fd65aa54f1678ed52d
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-17 10:53:56 -0300

    Carregando transaçoes da api fake

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,45 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          type: 'deposit',
+          category: 'Venda',
+          amount: 12000.00,
+          createdAt: new Date('2021-04-14')
+        },
+        {
+          id: 2,
+          title: 'Hambúrguer',
+          type: 'withdraw',
+          category: 'Alimentação',
+          amount: -59.00,
+          createdAt: new Date('2021-04-10')
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Despesas fixas',
+          amount: 1200.00,
+          createdAt: new Date('2021-04-05')
+        },
+        {
+          id: 4,
+          title: 'Venda de computador',
+          type: 'deposit',
+          category: 'Venda',
+          amount: 5400.50,
+          createdAt: new Date('2021-04-13')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'dtmoney/api';
 
