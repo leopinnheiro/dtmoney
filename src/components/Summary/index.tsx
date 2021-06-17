fe06bbbc031d2be8510e6292fedafd8525230ee7
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-17 12:12:07 -0300

    Criando contexto para centralizar alteraçoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 import { formatCurrency } from '../../utils/formats';
 
 import { Container } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
   return (
     <Container>
       <div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+//baseURL: 'https://leopinnheiro.github.io/dtmoney/api'
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/dtmoney/api'
+  baseURL: isDevelopment
+    ? 'http://localhost:3000/dtmoney/api' 
+    : 'https://leopinnheiro.github.io/dtmoney/api'
 })
